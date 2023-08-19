69c893fca25bfec275628890419c2e029abc9346
zehrasbr <zehraisbr@gmail.com>
2023-08-19 20:43:36 +0300

    Add project files.

---
+++ WeddingOrganization.PresantationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeddingOrganization.PresantationLayer.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
